5af8b2ad512e6130483bb0af36348c7b7c9b7b39
pablo <pavlospapadopou@gmail.com>
2023-03-01 14:05:50 +0200

    initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    // By extending from a plugin config, we can get recommended rules without having to add them manually.
+    'react-app',
+    'plugin:jsx-a11y/recommended',
+    // This disables the formatting rules in ESLint that Prettier is going to be responsible for handling.
+    // Make sure it's always the last config, so it gets the chance to override other configs.
+    'eslint-config-prettier',
+  ],
+  plugins: ['jsx-a11y'],
+  settings: {
+    react: {
+      // Tells eslint-plugin-react to automatically detect the version of React to use.
+      version: 'detect',
+    },
+    // Tells eslint how to resolve imports
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+        extensions: ['.js', '.jsx'],
+      },
+    },
+  },
+  rules: {
+    // Add your own rules here to override ones from the extended configs.
+    'react/react-in-jsx-scope': 'off',
+  },
+};
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>vite-react-project</p>
+    </div>
+  );
+}
+
+export default App;
